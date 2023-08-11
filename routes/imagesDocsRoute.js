fff005857ff1b1c504dc5ea5c83f92569f2c474c
Stritle <nuno_fernandes18@hotmail.com>
2023-08-11 15:15:54 +0100

    update router

--- routes/imagesDocsRoute.js
+++ routes/imagesDocsRoute.js
@@ -5,32 +5,31 @@ const router = express.Router();
 
 router.post("/storeDocs", async (req, res) => {
   const doc = new Docs({
-    base64Doc: req.body.imagesDocs,
-    nameDoc: req.body.docsName,
+    base64Doc: req.body.base64Doc,
+    nameDoc: req.body.nameDoc,
   });
   const newDoc = await doc.save();
-  // res.status(201).json({ message: "Documentos guardado com sucesso." });
   if (newDoc) {
     res.send({
       _id: newDoc.id,
-      base64Doc: newDoc.imagesDocs,
-      nameDoc: newDoc.docsName,
+      base64Doc: newDoc.base64Doc,
+      nameDoc: newDoc.nameDoc,
       token: getToken(newDoc),
     });
   } else res.status(401).send({ msg: "Erro ao guardar Documentos" });
 });
 //..................................
-router.get("/loadDocs", async (req, res) => {
-  const docs = await ImagesDocs.find();
-  const newDocs = await docs.save();
-  res.status(201).json({ message: "Documentos carregados com sucesso" });
-  if (newDocs) {
-    res.send({
-      imagesDocs: newDocs.imagesDocs,
-      nameDoc: newDocs.nameDoc,
-      token: getToken(newDocs),
-    });
-  } else res.status(401).send({ msg: "Erro ao carregar Documentos" });
-});
+// router.get("/loadDocs", async (req, res) => {
+//   const docs = await ImagesDocs.find();
+//   const newDocs = await docs.save();
+//   res.status(201).json({ message: "Documentos carregados com sucesso" });
+//   if (newDocs) {
+//     res.send({
+//       imagesDocs: newDocs.imagesDocs,
+//       nameDoc: newDocs.nameDoc,
+//       token: getToken(newDocs),
+//     });
+//   } else res.status(401).send({ msg: "Erro ao carregar Documentos" });
+// });
 
 export default router;
