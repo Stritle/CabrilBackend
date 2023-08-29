fec8f590e8b8c12ec4bd50536ef180bd21ce688a
Stritle <nuno_fernandes18@hotmail.com>
2023-08-29 17:51:45 +0100

    up

--- routes/imagesDocsRoute.js
+++ routes/imagesDocsRoute.js
@@ -21,7 +21,7 @@ router.post("/storeDocs", async (req, res) => {
 
 router.get("/loadDocs", async (req, res) => {
   try {
-    const documents = await Docs.find({});
+    const documents = await Docs.find();
     if (documents) {
       res.send(documents);
     } else {
