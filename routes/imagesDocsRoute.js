734f9fc06776f5b21f5fd5fdc56547026b6a8969
Stritle <nuno_fernandes18@hotmail.com>
2023-08-30 00:53:34 +0100

    up

--- routes/imagesDocsRoute.js
+++ routes/imagesDocsRoute.js
@@ -18,7 +18,7 @@ router.post("/storeDocs", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const userId = req.params.userId;
+  const userId = req.body.userId;
   const documents = await Docs.find(userId);
   if (documents) {
     console.log(documents);
