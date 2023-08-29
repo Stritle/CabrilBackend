8c2b621af20b88f76abdaa91f9ad8e786779b522
Stritle <nuno_fernandes18@hotmail.com>
2023-08-29 16:50:15 +0100

    up

--- routes/imagesDocsRoute.js
+++ routes/imagesDocsRoute.js
@@ -19,7 +19,7 @@ router.post("/storeDocs", async (req, res) => {
   } else res.status(401).send({ msg: "Erro ao guardar Documentos" });
 });
 
-router.get("/loadDocs", async (req, res) => {
+router.get("/loadDocs", isAuth, async (req, res) => {
   try {
     const documents = await Docs.find({ user: req.user._id });
     if (documents) {
